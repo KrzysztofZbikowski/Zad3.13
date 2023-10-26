004c81a43ddc0c0b46db50f2696889411b842dc9
Zbiku <Zbiku@MSI>
2023-10-26 22:50:34 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        int n, suma=0, start=1;
+        Console.WriteLine("Podaj liczbę naturalną");
+        n = int.Parse(Console.ReadLine());
+        for (int i=0;i<n;i++)
+        {
+            if (start%2!=0)
+            {
+                suma = suma + start;
+                start++;
+            }
+            else
+            {
+                suma = suma - start;
+                start++;
+            }
+        }
+        Console.WriteLine("Suma twojego ciągu wynosi: " + suma);
+        Console.ReadKey();
+    }
+}
